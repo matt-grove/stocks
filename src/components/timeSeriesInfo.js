9c2b1792b39bd00ba4fdb45ed0c93e43a769b0e5
Matt <matthewgrove95@gmail.com>
2020-02-26 11:41:38 +0000

    Line Graph - Props Tidyup

--- src/components/timeSeriesInfo.js
+++ src/components/timeSeriesInfo.js
@@ -1,60 +1,46 @@
 const colors = { '5min': '#13E2C9' };
+const getColor = line => colors[line.id];
 
-export const timeSeriesInfo = new Object();
-export const getColor = line => colors[line.id];
-
-timeSeriesInfo.lineWidth = 3.5;
-timeSeriesInfo.colors = getColor;
-timeSeriesInfo.enableArea = false;
-timeSeriesInfo.enablePoints = false;
-timeSeriesInfo.enableGridX= false;
-timeSeriesInfo.enableGridY= false;
-timeSeriesInfo.enableSlices= 'x';
-timeSeriesInfo.isInteractive= true;
-timeSeriesInfo.margin = {
-  top: 50,
-  right: 50,
-  bottom: 50,
-  left: 60
-};
-timeSeriesInfo.yScale = {
-  type: 'linear',
-  min: 'auto',
-  max: 'auto',
-  stacked: false,
-  reverse: false
-};
-timeSeriesInfo.xScale= {
-   type: "time",
-   format: "%Y-%m-%d %H:%M:%S",
-   precision: "second"
-};
-timeSeriesInfo.axisBottom= {
-  format: "%H:%M",
-  tickValues: "every 1 hours"
-};
-
-
-
-
-
-
-
-
-
-
-timeSeriesInfo.defs = [{
-  id: 'gradientA',
-  type: 'linearGradient',
-  colors: [
-      { offset: 0, color: '#13E2C9' },
-      { offset: 100, color: '#13E2C9' },
-  ]
-}]
-
-timeSeriesInfo.fill = {
-  match: { id: 'react' }, id: 'gradientA'
+const lineProperties = {
+  colors: getColor,
+  motionDamping: 18,
+  lineWidth: 3.5,
+  enableArea: false,
+  enablePoints: false,
+  enableGridX: false,
+  enableGridY: false,
+  enableSlices: 'x',
+  isInteractive: true,
+  motionStiffness: 300,
+  motionDamping: 18,
+  margin: {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 60
+  },
+  yScale: {
+    type: 'linear',
+    min: 'auto',
+    max: 'auto',
+    stacked: false,
+    reverse: false
+  },
+  xScale: {
+     type: "time",
+     format: "%Y-%m-%d %H:%M:%S",
+     precision: "second"
+  },
+  axisBottom:{
+    format: "%H:%M",
+    tickValues: "every 1 hours"
+  }
 }
+
+export default lineProperties;
+
+
+
 
 
 //
