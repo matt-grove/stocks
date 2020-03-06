b083ca1011844294d58a0e1b81941c2ad68155d6
Matt <matthewgrove95@gmail.com>
2020-03-06 16:53:32 +0000

    Structure Outline

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './App.css'
+import Header from './components/header'
+import Sidebar from './components/sidebar'
 import Main from './components/main'
 
 const App = () => {
   return (
-    <Main/>
+    <>
+      <Header/>
+      <Sidebar/>
+      <Main/>
+    </>
   );
 }
 
---
+++ src/components/sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Sidebar = (props) => {
+  return (
+    <div />
+  )
+}
+
+export default Sidebar
